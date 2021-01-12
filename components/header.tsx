23cc11f17b9d57cd3c84a9c6f05772cb89a703cb
Samanta Lara <slaragiannone@gmail.com>
2021-01-12 16:20:06 +0000

    make it better

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,30 @@
 import Link from 'next/link'
+import Head from 'next/head'
 
 const Header = () => {
     return (
-        <header className="header" >
-            <Link href="/">
-                <img src="/static/logo.svg" alt="" className="header__logo" />
-            </Link>
-            <Link href="/">
-                <h1 className="header__title">FishCastle</h1>
-            </Link>
-            <Link href="/products">
-                <h2 className="header__title">Products</h2>
-            </Link>
-            <a className="header__summary snipcart-checkout snipcart-summary" href="#" style={{ textDecoration: "none" }}>
-                <span className="header__price snipcart-total-price"></span>
-            </a>
-        </header>
+        <>
+            <Head>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+                <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="MDI2OTQxOTItZTg3MS00ZGYyLWEyYjQtYmQxYWNmYzgzNzdlNjM3NDYwNDEwODgzODM3NDU4" id="snipcart"></script>
+                <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
+            </Head>
+            <header className="header" >
+
+                <Link href="/">
+                    <img src="/static/logo.svg" alt="" className="header__logo" />
+                </Link>
+                <Link href="/">
+                    <h1 className="header__title">FishCastle</h1>
+                </Link>
+                <Link href="/products">
+                    <h2 className="header__title">Products</h2>
+                </Link>
+                <a className="header__summary snipcart-checkout snipcart-summary" href="#" style={{ textDecoration: "none" }}>
+                    <span className="header__price snipcart-total-price"></span>
+                </a>
+            </header>
+        </>
     );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,9 @@
-import Head from 'next/Head'
 import Layout from '../components/layout'
-import ProductList from '../components/productList'
-import { IProduct } from '../interfaces/product'
-import { IProductListProps } from '../interfaces/productListProps'
 import '../styless.scss'
 
 const Index = () => {
     return (
         <div className="app">
-            <Head>
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-                <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="MDI2OTQxOTItZTg3MS00ZGYyLWEyYjQtYmQxYWNmYzgzNzdlNjM3NDYwNDEwODgzODM3NDU4" id="snipcart"></script>
-                <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
-            </Head>
-
             <Layout>
                 <h2>Home</h2>
             </Layout>
