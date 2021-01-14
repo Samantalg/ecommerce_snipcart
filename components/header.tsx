8effba8f61b3a033a046d1e59e59b3d59e023474
Samanta Lara <slaragiannone@gmail.com>
2021-01-14 13:00:22 +0000

    hidden head

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
         <>
             <Head>
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-                <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="MDI2OTQxOTItZTg3MS00ZGYyLWEyYjQtYmQxYWNmYzgzNzdlNjM3NDYwNDEwODgzODM3NDU4" id="snipcart"></script>
+                <script hidden src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="MDI2OTQxOTItZTg3MS00ZGYyLWEyYjQtYmQxYWNmYzgzNzdlNjM3NDYwNDEwODgzODM3NDU4" id="snipcart"></script>
                 <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
             </Head>
             <header className="header" >
